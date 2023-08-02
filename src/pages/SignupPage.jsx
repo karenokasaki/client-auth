f271882791221208c9b1ddbd976df5c66407edb7
karen okasaki <oks.karen@gmail.com>
2023-08-01 21:01:20 -0300

    adding form login

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,10 +1,83 @@
+import { useState } from "react";
+import axios from "axios";
 
 function SignupPage() {
-   return ( 
+   const [form, setForm] = useState({
+      name: "",
+      email: "",
+      telefone: "",
+      password: "",
+   });
+
+   // controll input
+   function handleChange(e) {
+      setForm({ ...form, [e.target.name]: e.target.value });
+   }
+
+   async function handleSubmit(e) {
+      e.preventDefault();
+      try {
+         //lógica de submit do form
+
+         const response = await axios.post("http://localhost:4000/user/signup", form);
+
+         console.log(response);
+      } catch (error) {
+         // lógico se der erro na requisição
+      }
+   }
+
+   console.log(form);
+
+   return (
       <div>
          <h1>Signup Page</h1>
+
+         <form onSubmit={handleSubmit}>
+            <div>
+               <label>Nome Completo</label>
+               <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+               />
+            </div>
+
+            <div>
+               <label>Email</label>
+               <input
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+               />
+            </div>
+
+            <div>
+               <label>Telefone</label>
+               <input
+                  type="tel"
+                  name="telefone"
+                  value={form.telefone}
+                  onChange={handleChange}
+               />
+            </div>
+
+            <div>
+               <label>Senha</label>
+               <input
+                  type="password"
+                  name="password"
+                  value={form.password}
+                  onChange={handleChange}
+               />
+            </div>
+
+            <button>CADASTRE-SE</button>
+         </form>
       </div>
-    );
+   );
 }
 
 export default SignupPage;
