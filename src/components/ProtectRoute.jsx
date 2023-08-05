531c11a56f7c24f64505947f374a677f7df5d4f9
karen okasaki <oks.karen@gmail.com>
2023-08-05 17:04:30 -0300

    Rota protegida

--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -1,15 +1,16 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function ProtectRoute({ Component }) {
    const navigate = useNavigate();
 
    const token = localStorage.getItem("userToken");
 
-   if (token) {
-      //se o token existir vamos mostrar o componente
-      return <Component />;
-   } else {
-      // se o token não existir, enviar o usuário para a tela de login
-      navigate("/login");
-   }
+   useEffect(() => {
+      if (!token) {
+         navigate("/login");
+      }
+   }, [navigate, token]);
+
+   return <Component />;
 }
